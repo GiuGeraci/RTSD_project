590aed7e17fe44ec5597fdb9aae75135bc2a6767
ste__cal <stefano.caldarini97@gmail.com>
2020-11-27 18:35:28 +0100

    Pushami e pullami sta minchia

--- Blinky.c
+++ Blinky.c
@@ -6,6 +6,28 @@
 #include <stm32f10x.h>                       /* STM32F103 definitions         */
 #define GIGI_USE_ODR 999
 
+#define EMERGENCY_INPUT_PIN 1<<0
+#define STOP_INPUT_PIN 1<<1
+#define BRAKE_3_INPUT 1<<2
+#define BRAKE_2_INPUT 1<<3
+#define BRAKE_1_INPUT 1<<4
+#define IDLE_INPUT 1<<5
+#define ACCELERATION_1_INPUT 1<<6
+#define ACCELERATION_2_INPUT 1<<7
+#define ACCELERATION_3_INPUT 1<<8
+
+#define EMERGENCY_BRAKE 1<<11
+#define BRAKE_3 1<<10
+#define BRAKE_2 1<<9
+#define BRAKE_1 1<<8
+#define ACCELERATION_3 1<<2
+#define ACCELERATION_2 1<<1
+#define ACCELERATION_1 1<<0
+
+#define IDLE 0<<0;
+
+
+
 int WAIT;
 
 /*----------------------------------------------------------------------------
@@ -19,6 +41,37 @@ void wait (void)  {
   WAIT = 0;
 }
 
+void set_acc_3(){
+	GPIOC->ODR = ACCELERATION_3;
+}
+
+void set_acc_2(){
+	GPIOC->ODR = ACCELERATION_2;
+}
+
+void set_acc_1(){
+	GPIOC->ODR = ACCELERATION_1;
+}
+
+void idle(){
+	GPIOC->ODR = IDLE;
+}
+
+void set_brake_1(){
+	GPIOC->ODR = BRAKE_1;
+}
+
+void set_brake_2(){
+	GPIOC->ODR = BRAKE_2;
+}
+
+void set_brake_3(){
+	GPIOC->ODR = BRAKE_3;
+}
+
+void emergency_brake(){
+	GPIOC->ODR = EMERGENCY_BRAKE;
+}
 
 /*----------------------------------------------------------------------------
   Main Program
@@ -28,20 +81,26 @@ int main(void) {
   unsigned int i;                            /* LED variable                  */
 
   RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;        /* Enable GPIOB clock            */
-  // equivalent to RCC->APB2ENR |= (1UL << 3);                
-  GPIOB->CRH    =  0x00000003;               /* PB.8 defined as Output   */
-	i = 1<<8;
-#ifdef GIGI_USE_ODR
-  while (1) {		// Loop forever
-      GPIOB->ODR ^= i;                       // Toggle LED
-      wait ();                               /* call wait function            */
-  }
-#else
-  while (1) {		// Loop forever
-      GPIOB->BSRR = i;                       /* Turn LED on                   */
-      wait ();                               /* call wait function            */
-      GPIOB->BRR = i;                        /* Turn LED off                  */
-      wait ();                               /* call wait function            */
-  }
-#endif
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;					/* Enable GPIOC clock            */
+	            
+  GPIOB->CRL    =  0x88888888;
+	GPIOB->CRH    =  0x00000008;
+	GPIOC->CRL    =  0x00000333;
+	GPIOC->CRH    =  0x00003333;
+	
+	idle();
+	
+	while (1) {
+		if(GPIOB->IDR & IDLE_INPUT) {
+			idle();
+		}else if(GPIOB->IDR & BRAKE_1_INPUT){
+			set_brake_1();
+		}
+		wait ();
+	}
+	
 }
+	
+	
+	
+	
